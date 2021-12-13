b7cb9cc36af1f4684973205df9e3b23911aa7928
Clinic-Directory-2021 <89154737+Clinic-Directory-2021@users.noreply.github.com>
2021-12-13 22:22:32 +0800

    Resident

--- main_app/static/js/view_resident.js
+++ main_app/static/js/view_resident.js
@@ -1,7 +1,7 @@
 
 function viewResident(resident_id){
 
-    var url = "/resident_profile";
+    var url = "../resident_profile";
 
     // Construct the full URL with "id"
     document.location.href = url + "?resident_id=" + resident_id;
